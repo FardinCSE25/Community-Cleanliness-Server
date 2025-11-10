80ab629da963fca7c6842bcaf3a27e4b2d29a6f8
Fardin Ahmed <fardin.cse25@gmail.com>
2025-11-11 00:29:25 +0600

    wait

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("community-cleanliness-db");
     const issuesCollection = db.collection("issues");
@@ -91,7 +91,7 @@ async function run() {
       res.send(result);
     });
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
